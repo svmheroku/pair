6eebadc6fe1b02b4fb7dd5b6f20c9ed7892b23c0
oracle <oracle@zu.(none)>
2011-05-30 03:22:09 +0100

    c

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Svm::Application.config.secret_token = '88ac95b32608c06b91fbe063909434944cefa076dfd23b26c1c149c4e307bc12427864a85dc70b66d18811ce864655d7aff48be9a643f8b0f24615c3f2dd1cef'
